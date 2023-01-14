60377525064c07f9eb2d1a0021f2704bb195b1fd
elavarasan <elavarasa.003@gmail.com>
2023-01-14 09:20:21 +0000

    first

---
+++ src/data/BlogData.js
@@ -0,0 +1,22 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Coming Soon",
+        tags:["react","gsap","styled-components"],
+        date:"13 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
+    },
+    {
+        id:2,
+        name:"Coming Soon",
+        tags:[""],
+        date:"coming soon",
+        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+    },
+    
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,15 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Coming soon",
+        description:"",
+        tags:["waitOnSee"],
+        Image:"../assets/Images/profile-img.png",
+        demo:"https://www.instagram.com/follow.prince/",
+        github:"https://github.com/follow-prince"
+    },
+    
+  
+] 
